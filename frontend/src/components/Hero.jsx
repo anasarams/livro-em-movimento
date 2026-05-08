1f19c3c32c6d436355eda05c4b2e8bdc375f7836
anasarams <tvanasara@gmail.com>
2026-05-07 23:03:48 -0300

    alteração no Hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,28 +1,47 @@
+import heroImage from '../assets/trnava-university-BEEyeib-am8-unsplash.jpg'
+
 export default function Hero() {
   return (
-    <section className="bg-[#C1DFF8] px-10 py-16 flex flex-col md:flex-row items-center justify-between gap-10">
-      <div className="max-w-xl">
-    <h2 className="font-[Gloock] text-5xl font-bold mb-6">
-       Transformando vidas através da leitura
-      </h2>
+    <section
+      className="relative px-10 py-24 flex flex-col md:flex-row items-center justify-between gap-10 overflow-hidden"
+    >
 
-        <p className="text-gray-700 mb-8 text-lg">
-          O projeto Livro em Movimento promove o acesso à leitura e educação
-          para comunidades carentes.
+      
+      <img
+        src={heroImage}
+        alt="Livros e leitura"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+
+     
+      <div className="absolute inset-0 bg-black/40"></div>
+
+    
+      <div className="relative z-10 max-w-xl text-white">
+
+        <h2 className="font-[Gloock] text-5xl font-bold mb-6">
+          Transformando vidas através da leitura
+        </h2>
+
+        <p className="mb-8 text-lg leading-relaxed">
+          O projeto Livro em Movimento promove o acesso à leitura
+          e educação para comunidades carentes.
         </p>
 
         <div className="flex gap-4">
-        <button className="font-[Gloock] bg-white text-[#1B56AE] px-6 py-2 rounded-lg shadow hover:bg-[#1B56AE] hover:text-white hover:shadow-xl">
-           Doar
-         </button>
 
-         <button className="font-[Gloock] bg-white text-[#1B56AE] px-6 py-2 rounded-lg shadow hover:bg-[#1B56AE] hover:text-white hover:shadow-xl">
-          Contato
-         </button>
+          <button className="font-[Gloock] bg-white text-[#1B56AE] px-6 py-2 rounded-lg shadow hover:bg-[#1B56AE] hover:text-white hover:shadow-xl transition">
+            Doar
+          </button>
+
+          <button className="font-[Gloock] bg-white text-[#1B56AE] px-6 py-2 rounded-lg shadow hover:bg-[#1B56AE] hover:text-white hover:shadow-xl transition">
+            Contato
+          </button>
+
         </div>
+
       </div>
 
-      <div className="w-full md:w-[500px] h-[250px] bg-[#1B56AE] rounded-[35px] shadow-2xl"></div>
     </section>
   )
 }
